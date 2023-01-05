73cb393fe0e4d6d9a4fc4d738f03943a8723a666
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-01-05 23:27:22 +0700

    Update function.php

--- themes/oengine-admin/function.php
+++ themes/oengine-admin/function.php
@@ -1,5 +1,5 @@
 <?php
 
-add_asset_js(asset('themes/gate-admin/js/gate-admin.js'), '',  10);
-add_asset_css(asset('themes/gate-admin/css/gate-admin.css'), '', 10);
-add_page_body_class('gate-admin');
+add_asset_js(asset('themes/oengine-admin/js/oengine-admin.js'), '',  10);
+add_asset_css(asset('themes/oengine-admin/css/oengine-admin.css'), '', 10);
+add_page_body_class('oengine-admin');
